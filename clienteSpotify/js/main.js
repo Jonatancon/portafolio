f8805cb4396253b6392a65cb8ae8dbac76e74f6f
Jonatancon <jhonatanlora2011@gmail.com>
2021-05-30 16:23:37 -0500

    spotify projet
    todo full

---
+++ clienteSpotify/js/main.js
@@ -0,0 +1,340 @@
+const contentP = {
+  1: 'A genre-bending pop trio from New York City, Fun. infuse their literate indie rock songwriting sensibility with such varied influences as vintage 50s pop music, 80s adult contemporary, and hip-hop. Born out of three different bands, Fun. feature the talents of former  lead vocalist ,  guitarist , and  keyboardist . Fun. broke big in 2012 with their sophomore album, Some Nights, which topped the  Hot 100 chart for over six weeks and earned them two Grammy Awards.',
+  2: "Australian indie folk singer/songwriter Vance Joy began his career playing the Melbourne open-mike scene at the end of the 2000s while completing his law degree. Finding success with his intimate Bon Iver-influenced songs, Joy decided to put law on hold and pursue a music career instead. Born James Keogh, the singer took the name Vance Joy from a character in Peter Carey's novel Bliss in order to better separate his personal and professional life. As his reputation grew, he began working with a manager, booking bigger shows around town and eventually recording his debut EP God Loves You When You're Dancing. His catchy folk-pop single 'Riptide' was an immediate success in Australia and eventually led him to sign with Atlantic Records. In 2013, Joy began touring internationally and recording his first album for Atlantic. His debut Dream Your Life Away arrived in September 2014, spawning further hits Mess is Mine, Georgia and Fire and the Flood. His second album 'Nation of Two' was released in February 2018 featuring singles Lay It On Me, Saturday Sun and We're Going Home. www.vancejoy.com",
+  3: "BANKS, OR “A famous folk rock band is filming a music video in Banks, and it may involve a bloody man walking down a street and someone setting a piano on fire.” - KPTV  released their new album, III, on September 13, 2019 – the band's first full-length album since Cleopatra. III is audacious: every note, every syllable and every moment of silence in between is emotionally charged. The new album is a cinematic narrative told in three chapters, with the songs from each chapter focusing on one primary character in the fictious Spark’s family. “Gloria”, the first single from III, is about love between an addict and her family. “Jolting and unsparing” – Jon Pareles, New York Times Wesley Schultz says the album “was inspired by a member of my family, and no amount love or resources could save her. She’s now been homeless for over a year. Loving an addict is like standing among the crashing waves, trying to bend the will of the sea.”  says, “This collection of songs worked out in a beautiful way, and I feel with this album we’ve really hit our stride.” The band has sold out tours across the world, with over 300,000 tickets sold on 2017’s Cleopatra World Tour and ended with 27 sold out arena shows.  have also supported icons and their musical heroes on the road, including  on his final tour and  on its Joshua",
+  4: "hey hey it's ben, tom, and dillon. we make tunes from our house in our band called tipling rock. our friend liam joins us on guitar live. stay awhile",
+  5: "Soy Jósean Log, terrícola enamorado de la vida. Crecí en Cholula, un pintoresco pueblo mexicano donde una gran pirámide de 2,500 años domina el paisaje a la sombra del volcán Popocatépetl. La música es mi idioma, mi forma de vivir. Desde que tengo memoria, es a través de ella como mejor he conseguido entenderme en un mundo lleno de misterios fascinantes. Soy asustadizo y precavido, y suelo complicarme la existencia con dilemas que terminan obligándome a fluir y a reinventarme continuamente. No creo en las causas perdidas y, aunque veo el vaso “medio lleno”, creo que es nuestra responsabilidad llenar la otra mitad y compartirlo. Amo la naturaleza y amo a los animales, por eso soy herbívoro. Soy un idealista y tiendo a enredarme en discusiones filosóficas. Creo que el mundo puede ser un lugar mejor para todxs y sueño con que construyamos una comunidad global en armonía y en paz, donde la diversidad sea celebrada y donde todos los seres que habitamos este planeta tengamos la oportunidad de gozar nuestra existencia en un ambiente amigable. Desde niño me formé como baterista y las percusiones siempre han sido mi fuerte, pero luego de experimentar con otros instrumentos como el violín y el piano, descubrí a mi mejor aliado en el ukulele, con quien he aprendido a abrazar la voz que tengo y a cantar con sinceridad las inquietudes de mi alma.",
+  6: "Chaos Chaos is a Swedish-Mexican-American power sister-duo, with sisters Asy and Chloe Saavedra. The duo started out their musical journey as Smoosh in Seattle during at ages 8 and 10.  In 2004, Chloe and Asy debuted their first full-length album, She Like Electric, under the moniker of Smoosh. They released several songs under Smoosh until 2012. They renamed Smoosh to Chaos Chaos in 2012 to indicate a change. The duo would release their first EP in October 2012, S, as Chaos Chaos.  The new name ‘Chaos Chaos’ is taken from an amoeba which is the largest of its kind, Chaos carolinensis, as revealed in an interview with Interview Magazine; they also explained how they look forward on constantly changing their music as an amoeba changes its shape.  Chaos' October 2014 track “Do You Feel It?” got featured in Rick and Morty, drawing new listeners toward the band. Shortly after the sisters created 'Terryfold' with Justin Roiland, which reached number 33 on 'Hot Rock Songs'. The duo followed this up with the release of their critically acclaimed self-titled album, Chaos Chaos, in May 2018.",
+  7: "No singer of the 2010s could compare to Adele in terms of sheer sales or stardom. Beginning with 21, her sophomore set from 2011, Adele existed in a different stratosphere than her British or American peers, selling tens of millions of her albums at a time when many musicians scrambled to get to a million. The key to Adele's appeal lay in her sly, subtle interweaving of old-fashioned soul, confessional songwriting, and pop polish -- diverse sounds tied together by her powerhouse vocals. All of this was evident on her 2008 debut 19 (named, like her subsequent albums, after her age upon the time of its creation) and its U.K. Top 10 single Chasing Pavements, but it was 21 and its hit singles Rolling in the Deep, Someone Like You, and Set Fire to the Rain that established her as an international phenomenon.",
+  8: "Coldplay came together as a band in late 1997 when Guy Berryman, Jonny Buckland, Will Champion and Chris Martin met at University College, London. The band have gone on to become one of the planet’s most popular acts, selling more than 75 million copies of their seven Number One albums, which have spawned a string of classic hits including Yellow, Clocks, Fix You, Paradise, Viva La Vida and A Sky Full Of Stars. They’ve also filled their trophy cabinet with almost every major music award there is (including seven Grammys and nine BRITs).  In December 2015, the band released A Head Full Of Dreams, their seventh album, which included the new fan favourites Adventure Of A Lifetime and Hymn For The Weekend. A Head Full Of Dreams has since sold more than 6 million copies and racked up well over a billion streams on Spotify.  The band released the Grammy-nominated Kaleidoscope EP in July 2017. The EP included Something Just Like This, a collaboration with The Chainsmokers which became one of the band’s biggest hits.  In November 2017, Coldplay concluded their spectacular A Head Full Of Dreams Tour. Over 18 months, the tour saw the band perform to more than five million fans around the world, in what is now officially recognised as the third biggest tour of all-time.  In Nov 2019, the band released their Grammy-nominated 8th album Everyday Life.",
+  9: "Ed Sheeran may be the quintessential pop star of the 2010s: a singer/songwriter who seems to acknowledge no boundaries between styles or eras, creating a sound that's idiosyncratic and personal. Sheeran borrows from any style that crosses his path -- elements of folk, hip-hop, pop, dance, soul, and rock can be heard in his big hits The A Team, Sing, Thinking Out Loud, and Shape of You -- which gives him a broad appeal among different demographics, helping to elevate him to international stardom not long after the release of his 2011 debut, +.",
+  10: "By cannily synthesizing many of the sonic signifiers of alternative rock, Cage the Elephant became one of the biggest rock bands of the 2010s. The Kentucky natives relocated to England just before the 2008 release of their eponymous debut, and the ensuing media boost resulted in the transatlantic hit Aint No Rest for the Wicked, a clever pop tune that sounded like a hybrid between  and . The group soon became a reliable fixture on American modern rock radio, with two albums going gold (2011s Thank You Happy Birthday and 2013s Melophobia), and a Grammy Award for their 2015 album, the -produced Tell Me Im Pretty. It was all proof that Cage the Elephant had become one of the rare popular mainstream rock groups of their time.",
+  11: 'Paramore is a band that formed in 2003 in a quiet town that is now known as Nashville, TN. Still in junior high, music made up 100% of their extracurricular time. Until eventually, they all left for tour in the middle of a school year and thank heaven music would eventually work out for them. No one understood how a band so un-country could be from this strange little town called Nashville, nor how a bunch of kids so small could play their angst-filled songs so proudly.',
+  12: "Manuel Medrano is an award-winning pop singer and songwriter from Colombia. Born in Cartagena de Indias in 1987, his musical career appears to have been destined rather than deliberately pursued -- at least initially. After missing a year of high school due to youthful mishaps, he was punished with the gift of a guitar. He taught himself to play and began writing songs at 16, observing the folk and pop styles of his country. He learned his craft by playing Bogota's bars for several years while continuing to compose. Some of the songs written during this period became hits later."
+}
+let num = 12;
+const artist = {
+  1: '5nCi3BB41mBaMH9gfr6Su0',
+  2: '10exVja0key0uqUkk6LJRT',
+  3: '16oZKvXb6WkQlVAjwo2Wbg',
+  4: '2ZDHVJnLtH2KJfBFzShihr',
+  5: '1LMyTeRhjaitILs98h3MaF',
+  6: '6D6rjLdxyE5vwhMlkuQq0E',
+  7: '4dpARuHxo51G3z768sgnrY',
+  8: '4gzpq5DPGxSnKTe4SA8HAU',
+  9: '6eUKZXaKkcviH0Ku9w2n3V',
+  10: '26T3LtbuGT1Fu9m0eRq5X3',
+  11: '74XFHRwlV6OrjEM0A2NCMF',
+  12: '0i5iO6icb7kxg48thi9gBM'
+}
+//-----------------------------------------------------------//
+const APIController = (function () {
+
+  const clientId = 'b83a976f539345e9ad407434ab8f7edf';
+  const clientSecret = '781cfe41dee046c2980423dec9456278';
+
+  //private method
+  const _getToken = async () => {
+
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+      },
+      body: 'grant_type=client_credentials'
+    });
+
+    const data = await result.json();
+    return `${data.token_type} ${data.access_token}`;
+  }
+
+  const _getTopTracks = async (token, artisName) => {
+
+    const result = await fetch(`https://api.spotify.com/v1/artists/${artisName}/top-tracks?market=US`, {
+      method: 'GET',
+      headers: { 'Authorization': token }
+    });
+    const data = await result.json();
+    return data;
+  }
+
+  const _getArtist = async (token, artistName) => {
+
+    const result = await fetch(`https://api.spotify.com/v1/artists/${artistName}`, {
+      method: 'GET',
+      headers: { 'Authorization': token }
+    });
+    const data = await result.json();
+    return data;
+
+  }
+
+  return {
+
+    getToken() {
+      return _getToken();
+    },
+    getTopTracks(token, artisName) {
+      return _getTopTracks(token, artisName);
+    },
+    getArtist(token, artistName) {
+      return _getArtist(token, artistName);
+    }
+
+  }
+
+})();
+//----------------------------------------------------------//
+const dataPreprocessing = (function () {
+
+  const _topTracksDepuring = (data) => {
+
+    let tracks = data.tracks;
+    let dataFilter = tracks.map(function (track) {
+      return {
+        name: track.name,
+        nameAlbum: track.album.name,
+        image: track.album.images[0].url,
+        audio: track.preview_url,
+        popularity: track.popularity
+      }
+    });
+    return dataFilter;
+  }
+
+  return {
+    topTracksDepuring(data) {
+      return _topTracksDepuring(data);
+    }
+  }
+
+})();
+//-----------------------------------------------------------//
+const UIController = (function () {
+
+  const printTopTracks = (dataFilter) => {
+
+    let rootContainer = document.getElementById('primal');
+    dataFilter.map(function (track) {
+
+      let colm = document.createElement("div");
+      let card = document.createElement("div");
+      let cardHeader = document.createElement("div");
+      let cardFooter = document.createElement("div");
+      let img = document.createElement("img");
+      let cardBody = document.createElement("div");
+      let h5 = document.createElement("h5");
+      let p = document.createElement("p");
+      let audio = document.createElement("audio");
+      let popular = document.createElement("h2");
+
+      colm.classList.add("col-md-6");
+      colm.classList.add("mb-3");
+      colm.classList.add("card-main");
+      card.classList.add("border-success");
+      card.classList.add("card");
+      cardHeader.classList.add("card-header");
+      cardHeader.classList.add("bg-transparent");
+      cardHeader.classList.add("border-success");
+      cardFooter.classList.add("card-footer");
+      cardFooter.classList.add("bg-transparent");
+      cardFooter.classList.add("border-success");
+      img.classList.add("card-img-top");
+      img.classList.add("img-main");
+      cardBody.classList.add("card-body");
+      h5.classList.add("card-title");
+      h5.classList.add("text-end");
+      popular.classList.add("badge");
+      popular.classList.add("bg-danger");
+      popular.classList.add("text-wrap");
+      p.classList.add("card-text");
+      p.classList.add("text-end");
+      audio.classList.add("w-100");
+
+      img.src = track.image;
+      h5.textContent = track.name;
+      p.textContent = `ALBUM: ${track.nameAlbum}`
+      audio.src = track.audio;
+      audio.controls = "controls";
+      popular.textContent = `${track.popularity} %`;
+      colm.id = 'secondP';
+
+      cardHeader.appendChild(popular);
+      cardHeader.appendChild(h5);
+      cardHeader.appendChild(p);
+      cardFooter.appendChild(audio);
+      cardBody.appendChild(img);
+      card.appendChild(cardHeader);
+      card.appendChild(cardBody);
+      card.appendChild(cardFooter);
+      colm.appendChild(card);
+
+      rootContainer.appendChild(colm);
+
+    });
+  }
+
+  const printArtist = (data) => {
+    let root = document.getElementById('name');
+    let photo = document.getElementById('photo');
+
+    let name = document.createElement('span');
+    let image = document.createElement('img');
+
+    name.classList.add('d-block');
+    image.classList.add('vance');
+    name.id = 'names';
+    image.id = 'image';
+
+    name.textContent = data.name;
+    image.src = data.images[0].url;
+
+    root.appendChild(name);
+    photo.appendChild(image);
+
+  }
+
+  const removeContent = () => {
+    let root = document.getElementById('names');
+    let photo = document.getElementById('image');
+    let rootContainer = document.getElementById('primal');
+    let main = document.getElementById('main');
+
+    root.parentNode.removeChild(root);
+    photo.parentNode.removeChild(photo);
+    rootContainer.parentNode.removeChild(rootContainer);
+
+    let newRoot = document.createElement("div");
+    newRoot.classList.add('row');
+    newRoot.classList.add('row-cols-1');
+    newRoot.classList.add('row-cols-md-2');
+    newRoot.classList.add('g-4');
+    newRoot.id = "primal";
+
+    main.appendChild(newRoot);
+  }
+
+  return {
+    printTopTracks(dataFilter) {
+      return printTopTracks(dataFilter);
+    },
+    printArtist(data) {
+      return printArtist(data);
+    },
+    removeContent() {
+      return removeContent();
+    }
+  }
+
+})();
+//----------------------------------------------------------//
+const APPController = (function (APICtrl, DTO, UICtrl) {
+
+  const load = async () => {
+
+    const art = artist[1];
+
+    let p = document.getElementById('parraf');
+    p.textContent = contentP[1];
+
+    const token = await APICtrl.getToken();
+    const dataArtist = await APICtrl.getArtist(token, art);
+    const topTracks = await APICtrl.getTopTracks(token, art);
+
+    const tracks = DTO.topTracksDepuring(topTracks);
+
+
+    UICtrl.printTopTracks(tracks);
+    UICtrl.printArtist(dataArtist);
+  }
+
+  const loadClick = async (artist) => {
+
+    UICtrl.removeContent();
+
+    let p = document.getElementById('parraf');
+    p.textContent = contentP[num];
+
+    const token = await APICtrl.getToken();
+    const dataArtist = await APICtrl.getArtist(token, artist);
+    const topTracks = await APICtrl.getTopTracks(token, artist);
+
+    const tracks = DTO.topTracksDepuring(topTracks);
+
+
+    UICtrl.printTopTracks(tracks);
+    UICtrl.printArtist(dataArtist);
+  }
+
+
+  return {
+    init() {
+      console.log('App is starting');
+      load();
+    },
+    loadClick(artist) {
+      return loadClick(artist);
+    }
+  }
+
+})(APIController, dataPreprocessing, UIController);
+//-------------------------------------------------------------------//
+APPController.init();
+
+
+document.getElementById("change").addEventListener('click', (e) => {
+  e.defaultPrevented;
+  function random(min) {
+    if (min <= 1) {
+      num = 12;
+    } else {
+      num--;
+    }
+    return num;
+  }
+  console.log(num);
+  APPController.loadClick(artist[random(num)]);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
